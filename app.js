e3cb52dd0a0e728e00331b599c648c839b9c56ce
Sunil Kumar M <sunilkumar.m080@gmail.com>
2022-03-11 11:51:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-var name = require('./routes/name');
+
 
 var app = express();
 
@@ -25,7 +25,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-app.use('/hello', name);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
