33fa5c11ec0e8837abefe86552054d137b674d50
Sunil Kumar M <sunilkumar.m080@gmail.com>
2022-03-11 11:50:59 +0530

    Update index.js
    Name End Point

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express powered by Plesk' });
+  res.render('index', { title: 'WelCome To Red Hat Node Js Application' });
+});
+
+router.get('/hello', function(req, res, next) {
+  res.send('Hello My Name is Sunil Kumar M');
+  res.render('index', { title: 'Name' });
 });
 
 module.exports = router;
